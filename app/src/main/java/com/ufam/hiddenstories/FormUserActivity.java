39da2eb2baa73f65ae6610b9d8df4a3789725823
Raphael Rocha <raphaelrocha86@gmail.com>
2016-01-17 21:06:59 -0400

    criar usuário.

--- app/src/main/java/com/ufam/hiddenstories/FormUserActivity.java
+++ app/src/main/java/com/ufam/hiddenstories/FormUserActivity.java
@@ -1,5 +1,6 @@
 package com.ufam.hiddenstories;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,11 +12,14 @@ import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.VolleyError;
+import com.google.gson.Gson;
 import com.ufam.hiddenstories.conn.ServerInfo;
 import com.ufam.hiddenstories.conn.VolleyConnection;
 import com.ufam.hiddenstories.interfaces.CustomVolleyCallbackInterface;
+import com.ufam.hiddenstories.models.User;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -33,21 +37,6 @@ public class FormUserActivity extends BaseActivity implements CustomVolleyCallba
         setContentView(R.layout.activity_form_user);
 
         mToolbar  = setUpToolbar("Criar conta",true,false);
-        /*mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mToolbar.setTitle("Novo usuário");//texto temporário com o nome do local
-        setSupportActionBar(mToolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(false);
-
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-                finish();
-            }
-        });*/
 
         mVolleyConnection = new VolleyConnection(this);
 
@@ -60,43 +49,131 @@ public class FormUserActivity extends BaseActivity implements CustomVolleyCallba
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                save();
+                validade();
             }
         });
 
         hideKeyboard();
     }
 
-    private void save(){
+    private void validade(){
         String name = edtName.getText().toString().trim();
-        String login = edtLogin.getText().toString().trim();
+        String email = edtLogin.getText().toString().trim();
         String passwd = edtPasswd.getText().toString().trim();
         String passwd2 = edtPasswd2.getText().toString().trim();
 
+        boolean valid = true;
+        String msg = "";
+
+        if(name.equals("")){
+            valid = false;
+            if(msg.equals("")){
+                msg = msg+"\"Informe seu nome.";
+            }else{
+                msg = msg+"\n\"Informe seu nome.";
+            }
+        }
+
+        if(email.equals("")){
+            valid = false;
+            if(msg.equals("")){
+                msg = msg+"\"Informe seu e-mail.";
+            }else{
+                msg = msg+"\n\"Informe seu e-mail.";
+            }
+        }
+
+        if(passwd.equals("")){
+            valid = false;
+            if(msg.equals("")){
+                msg = msg+"\"Informe sua senha.";
+            }else{
+                msg = msg+"\n\"Informe sua senha.";
+            }
+        }
+
+        if(passwd2.equals("")){
+            valid = false;
+            if(msg.equals("")){
+                msg = msg+"\"Repita sua senha.";
+            }else{
+                msg = msg+"\n\"Repita sua senha.";
+            }
+        }
+
+        if(!passwd.equals(passwd2)){
+            valid = false;
+            if(msg.equals("")){
+                msg = msg+"\"As senhas devem ser iguais.";
+            }else{
+                msg = msg+"\n\"As senhas devem ser iguais.";
+            }
+        }
+
+        if(valid){
+            User u = new User();
+            u.setName(name);
+            u.setEmail(email);
+            u.setPasswd(passwd);
+            u.setPictureProfile("user.jpg");
+            save(u);
+        }else{
+            longAlert(msg);
+        }
+    }
+
+    private void save(User u){
+
         HashMap<String,String> params = new HashMap<String,String>();
-        params.put("name",name);
-        params.put("login",login);
-        params.put("passwd",passwd);
-        params.put("passwd2",passwd2);
+        params.put("name",u.getName());
+        params.put("email",u.getEmail());
+        params.put("passwd",u.getPasswd());
+        params.put("picture_profile",u.getPictureProfile());
 
         showLongSnack("Salvando novo usuário...");
 
-        //mVolleyConnection.callServerApiByJsonObjectRequest(ServerInfo.NEW_USER, Request.Method.POST, params, "NEW_USER");
+        mVolleyConnection.callServerApiByJsonObjectRequest(ServerInfo.CREATE_USER, Request.Method.POST, false, params, "CRIA_USUARIO");
+    }
+
+    public void setLogin(JSONObject jo){
+
+        User userLogged = null;
+        userLogged = popUser(jo);
+        getSession().setLogin(true);
+        getEditorPref().putBoolean("ul", true); // Storing boolean - true/false
+
+        Gson gson = new Gson();
+        String jsonUserLogged = gson.toJson(userLogged);
+
+        getEditorPref().putString("ul-obj", jsonUserLogged); // Storing string
+
+        getEditorPref().commit(); // commit changes
+        hideDialog();
+
+        Intent intent = new Intent(FormUserActivity.this, CategoryListActivity.class);
+        intent.putExtra("user",userLogged);
+        startActivity(intent);
+        hideDialog();
+        finish();
+    }
+
+    @Override
+    public void deliveryResponse(JSONArray response, String flag) {
 
     }
 
     @Override
-    public void deliveryResponse(JSONArray response, String TAG) {
-
+    public void deliveryResponse(JSONObject response, String flag) {
+        hideDialog();
+        try {
+            setLogin(response.getJSONObject("user"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void deliveryResponse(JSONObject response, String TAG) {
-
-    }
-
-    @Override
-    public void deliveryError(VolleyError error, String TAG) {
+    public void deliveryError(VolleyError error, String flag) {
 
     }
 }
